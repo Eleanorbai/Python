0d10b039029b247a41920fcede78f5f00a26011f
Eleanorbai <1554253403@qq.com>
2017-02-04 21:58:01 +0800

    Update pay.py

--- pay.py
+++ pay.py
@@ -1,7 +1,8 @@
+#I’m going to be using raw_input. But remember that hands a string in so
+#I’m going to have to use float.
 inp = raw_input("Enter your work hour:")
 hour = float(inp)
 inp = raw_input("Enter your work rate:")
 rate = float(inp)
 pay = hour * rate
 print pay
-
